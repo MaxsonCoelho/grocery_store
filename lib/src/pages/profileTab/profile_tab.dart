162ef6c0cd8454adba2547173354cf3232cb1ae7
Maxson Coelho <maxsoncoelho@gmail.com>
2023-08-03 19:30:25 -0400

    complete layout profile

--- lib/src/pages/profileTab/profile_tab.dart
+++ lib/src/pages/profileTab/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:grocery_store/src/pages/commom_widgets/custom_text_field.dart';
 import 'package:grocery_store/src/config/app_data.dart' as appData;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,26 +53,96 @@ class ProfileTab extends StatelessWidget {
             label: 'CPF',
             isSecret: true,
           ),
-
           SizedBox(
             height: 50,
             child: OutlinedButton(
               onPressed: () {
-                
+                updatePassword();
               },
               style: OutlinedButton.styleFrom(
-                side: const BorderSide(
-                  color: Colors.green,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)
-                )
-              ),
+                  side: const BorderSide(
+                    color: Colors.green,
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
               child: const Text('Atualizar senha'),
             ),
           )
         ],
       ),
     );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Atualização de senha',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const CustomFormField(
+                        isSecret: true,
+                        icon: Icons.lock,
+                        label: 'Senha atual',
+                      ),
+                      const CustomFormField(
+                        isSecret: true,
+                        icon: Icons.lock_outline,
+                        label: 'Nova senha',
+                      ),
+                      const CustomFormField(
+                        isSecret: true,
+                        icon: Icons.lock_outline,
+                        label: 'Confirmar senha',
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: const Text(
+                          'Atualizar',
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.close,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
